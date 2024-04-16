253c8dfb6e9f24859127c299945ed8dd27aa6f76
Caio Gabriel <cgfuentelima0277@gmail.com>
2024-04-15 22:12:53 -0300

    componentes de menu

--- client/src/Components/Dashboard/Dashboard.jsx
+++ client/src/Components/Dashboard/Dashboard.jsx
@@ -1,13 +1,24 @@
 import React from "react";
+import "../Dashboard/dashboard.css";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Work from "./Components/Work";
+import Testimonial from "./Components/Testimonial";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
+
 
 const Dashboard = () =>{
-    return(
-        <div>
-            pagina dashboard
-            <a href="/"> Log Out</a>
+    return (
+        <div className="App">
+          <Home />
+          <About />
+          <Work />
+          <Testimonial />
+          <Contact />
+          <Footer />
         </div>
-    )
-
-}
+      );
+    }
 
 export default Dashboard
